cd3fb2af02b6c3397b81fc83ad645995f6da144a
rodion1682 <rodiontropkin17@gmail.com>
2025-06-16 12:12:40 +0300

    move database, add gitignore to sql

--- server/config/connect.php
+++ server/config/connect.php
@@ -1,5 +1,6 @@
 <?php
-	$connect = mysqli_connect("127.127.126.25", "root", "", "crud");
+	//$connect = mysqli_connect("127.127.126.25", "root", "", "crud");
+	$connect = mysqli_connect("db", "root", "root", "crud");
 	if(!$connect) {
 		die('Unable to connect to database');
 	}
